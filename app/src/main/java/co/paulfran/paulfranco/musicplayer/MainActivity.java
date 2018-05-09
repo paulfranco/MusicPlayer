1207d83ad1c8ee0833560e2221b9b54a3ffcc94c
Paul Franco <paulfranco@me.com>
2018-05-09 01:47:54 -0700

    updated MainActivity and added Intents

--- app/src/main/java/co/paulfran/paulfranco/musicplayer/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/musicplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package co.paulfran.paulfranco.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRockMusicList(View view){
+        Intent intent = new Intent(this, RockActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCountryMusicList(View view){
+        Intent intent = new Intent(this, CountryActivity.class);
+        startActivity(intent);
+    }
+
+    public void openJazzMusicList(View view){
+        Intent intent = new Intent(this, JazzActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRapMusicList(View view){
+        Intent intent = new Intent(this, RapActivity.class);
+        startActivity(intent);
+    }
+
+    public void openClassicalMusicList(View view){
+        Intent intent = new Intent(this, ClassicalActivity.class);
+        startActivity(intent);
+    }
+
+    public void openGrungeMusicList(View view){
+        Intent intent = new Intent(this, GrungeActivity.class);
+        startActivity(intent);
     }
 }
